839e81a362c6926642b3a04bf985896f0b942e58
Alexander Linder <alexander.linder@mondcarion.group>
2024-01-30 08:47:52 +0100

    Add rudimentary pass parser

--- app/src/main/java/nz/eloque/foss_wallet/model/Pass.kt
+++ app/src/main/java/nz/eloque/foss_wallet/model/Pass.kt
@@ -1,35 +1,46 @@
 package nz.eloque.foss_wallet.model
 
 import android.location.Location
+import org.json.JSONArray
+import org.json.JSONObject
 import java.time.ZonedDateTime
 
 
 data class PassField(val key: String, private val label: String, private val value: String, val hide: Boolean, val hint:String = "")
 
-class Pass(val id: String) {
-    var creator: String = ""
+fun fromRaw(rawPass: RawPass): Pass {
+    return Pass().also { pass ->
+        pass.organization = rawPass.passJson.getString("organizationName")
+        pass.description = rawPass.passJson.getString("description")
+        pass.serialNumber = rawPass.passJson.getString("serialNumber")
+        forEach(rawPass.passJson.getJSONArray("locations")) { locJson ->
+            pass.locations.add(Location("").also {
+                it.latitude = locJson.getDouble("latitude")
+                it.longitude = locJson.getDouble("longitude")
+            })
+        }
+    }
+}
+
+fun forEach(jsonArray: JSONArray, action: (JSONObject) -> Unit) {
+    var i = 0
+    while (i < jsonArray.length()) {
+        val element = jsonArray.getJSONObject(i)
+        action.invoke(element)
+        i++
+    }
+}
+
+class Pass {
+    var organization: String? = null
     var type: PassType = PassType.EVENT
     var barCode: BarCode? = null
     var description: String? = null
-        get() {
-            if (field == null) {
-                return "" // better way of returning no description - so we can avoid optional / null checks and it is kind of the same thing
-                // an navigation_drawer_header description - we can do kind of all String operations safely this way and do not have to care about the existence of a real description
-                // if we want to know if one is there we can check length for being 0 still ( which we would have to do anyway for navigation_drawer_header descriptions )
-                // See no way at the moment where we would have to distinguish between an navigation_drawer_header and an missing description
-            }
-            return field
-        }
-
-    class TimeRepeat(val offset: Int, val count: Int)
-    class TimeSpan(val from: ZonedDateTime? = null, val to: ZonedDateTime? = null, val repeat: TimeRepeat? = null)
-    var validTimespans: List<TimeSpan> = ArrayList()
-    var calendarTimespan: TimeSpan? = null
+    var serialNumber: String? = null
+    class TimeSpan(val from: ZonedDateTime? = null, val to: ZonedDateTime? = null)
     var fields: MutableList<PassField> = ArrayList()
-    var locations: List<Location> = ArrayList()
+    var locations: MutableList<Location> = ArrayList()
     var app: String? = null
     var authToken: String? = null
     var webServiceURL: String? = null
-    var serial: String? = null
-    var passIdent: String? = null
 }
