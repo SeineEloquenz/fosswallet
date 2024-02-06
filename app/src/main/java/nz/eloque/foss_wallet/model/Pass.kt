0e53f8a03ba7511640b92d40b5a34463ebbc4779
Alexander Linder <alexander.linder@mondcarion.group>
2024-02-06 17:00:08 +0100

    Fix null strings being returned as empty strings
    This is a dirty hack, I don't get why it is necessary in the first place...

--- app/src/main/java/nz/eloque/foss_wallet/model/Pass.kt
+++ app/src/main/java/nz/eloque/foss_wallet/model/Pass.kt
@@ -39,11 +39,16 @@ data class Pass(
     var relevantDate: Long = 0
     var expirationDate: Long = 0
     var organization: String? = null
+        get() { return if (field == "") null else field } //TODO inspect why Room converts null to ""???
     var type: PassType = PassType.EVENT
     var serialNumber: String? = null
+        get() { return if (field == "") null else field }
     var authToken: String? = null
+        get() { return if (field == "") null else field }
     var webServiceUrl: String? = null
+        get() { return if (field == "") null else field }
     var passIdent: String? = null
+        get() { return if (field == "") null else field }
     var locations: MutableList<Location> = LinkedList()
     var headerFields: MutableList<PassField> = LinkedList()
     var primaryFields: MutableList<PassField> = LinkedList()
