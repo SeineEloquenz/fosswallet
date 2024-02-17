978f90be1775db21e7bfb6cc05f818ea7418f35e
Alexander Linder <alexander.linder@mondcarion.group>
2024-02-17 15:12:00 +0100

    Update navigation compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,7 +83,7 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.accompanist:accompanist-permissions:0.30.1")
 
 
