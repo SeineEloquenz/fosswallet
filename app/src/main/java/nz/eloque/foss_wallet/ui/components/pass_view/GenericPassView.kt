9cc72ebb5e6d33e9dba3f383b7fa26124ee0e62a
Alexander Linder <alexander.linder@mondcarion.group>
2024-02-22 14:13:47 +0100

    Add GenericPassPreview

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/pass_view/GenericPassView.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/pass_view/GenericPassView.kt
@@ -11,9 +11,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.Pass
+import nz.eloque.foss_wallet.model.PassType
 
 @Composable
 fun GenericPassView(
@@ -58,4 +60,41 @@ fun GenericPassView(
         AsyncPassImage(model = pass.logoFile(context), modifier = Modifier.fillMaxWidth())
         AsyncPassImage(model = pass.footerFile(context), modifier = Modifier.fillMaxWidth())
     }
+}
+
+@Preview
+@Composable
+private fun GenericPassPreview() {
+    val pass = Pass(
+        0,
+        "KSC - SV Elversberg",
+        1,
+        "KSC",
+        "serial",
+        PassType.BOARDING,
+        HashSet(),
+        false,
+        false,
+        false,
+        false
+    ).also {
+        it.relevantDate = 1800000000L
+        it.headerFields = mutableListOf(
+            nz.eloque.foss_wallet.model.PassField("block", "Block", "S1"),
+            nz.eloque.foss_wallet.model.PassField("seat", "Seat", "47"),
+        )
+        it.primaryFields = mutableListOf(
+            nz.eloque.foss_wallet.model.PassField("name", "Name", "Max Mustermann"),
+            nz.eloque.foss_wallet.model.PassField("seat", "Seat", "47"),
+        )
+        it.auxiliaryFields = mutableListOf(
+            nz.eloque.foss_wallet.model.PassField("block", "Block", "S1 | Gegengerade"),
+            nz.eloque.foss_wallet.model.PassField("seat", "Seat", "36E"),
+        )
+        it.secondaryFields = mutableListOf(
+            nz.eloque.foss_wallet.model.PassField("data1", "data1", "Longer Value here i guess"),
+            nz.eloque.foss_wallet.model.PassField("data2", "data2", "Shorter Value"),
+        )
+    }
+    GenericPassView(pass, true)
 }
