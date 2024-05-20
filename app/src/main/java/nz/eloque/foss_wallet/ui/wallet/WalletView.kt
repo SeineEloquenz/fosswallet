ee175663730addc22e6c6e2c25761659ca6bdb33
Alexander Linder <seine@eloque.nz>
2024-05-20 20:47:14 +0200

    Add logo hint if wallet is empty

--- app/src/main/java/nz/eloque/foss_wallet/ui/wallet/WalletView.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/wallet/WalletView.kt
@@ -1,20 +1,31 @@
 package nz.eloque.foss_wallet.ui.wallet
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Wallet
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.Pass
 import nz.eloque.foss_wallet.ui.components.PassCard
 
@@ -31,6 +42,22 @@ fun WalletView(
     val comparator by remember { mutableStateOf( Comparator<Pass> { left, right ->
         -left.id.compareTo(right.id)
     }) }
+
+    if (list.value.passes.isEmpty()) {
+        Box(modifier = modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                imageVector = Icons.Default.Wallet,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.secondary),
+                contentDescription = stringResource(R.string.wallet),
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth(0.5f),
+                alpha = 0.25f
+            )
+        }
+    }
+
     LazyColumn(
         state = listState,
         verticalArrangement = Arrangement
