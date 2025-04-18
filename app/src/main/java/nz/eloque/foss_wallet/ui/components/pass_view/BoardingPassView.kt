c4c7c107744c0fd771ebef888aadfcb4bb8cceef
Alexander Linder <seine@eloque.nz>
2025-04-18 15:06:32 +0200

    Show destination smaller if destination string is longer than an airport code

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/pass_view/BoardingPassView.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/pass_view/BoardingPassView.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nz.eloque.foss_wallet.R
@@ -103,7 +104,9 @@ fun DestinationCard(
     ) {
         Text(
             text = destination,
-            style = MaterialTheme.typography.headlineLarge,
+            style = if (destination.length <= 3) { MaterialTheme.typography.headlineLarge } else { MaterialTheme.typography.headlineSmall },
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
             textAlign = TextAlign.Center,
             modifier = Modifier
                 .padding(5.dp)
