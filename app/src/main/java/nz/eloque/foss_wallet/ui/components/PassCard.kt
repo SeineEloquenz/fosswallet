91def6b240147568ec052dd71eccf2607e06414b
Alexander Linder <seine@eloque.nz>
2025-04-18 23:12:37 +0200

    Render logo smaller on the left of the heading on PassCard

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/PassCard.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/PassCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -16,11 +17,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.PassField
-import nz.eloque.foss_wallet.ui.components.pass_view.AsyncPassImage
 import nz.eloque.foss_wallet.ui.components.pass_view.HeaderFieldsView
 import java.io.File
 
@@ -45,33 +49,39 @@ fun PassCard(
                 .fillMaxWidth()
         ) {
             Row(
-                horizontalArrangement = Arrangement.Start,
+                horizontalArrangement = Arrangement.Start
             ) {
                 Column(
                     modifier = Modifier.weight(5f)
                 ) {
-                    Text(
-                        text = description,
-                        overflow = TextOverflow.Ellipsis,
-                        maxLines = 1,
-                        style = MaterialTheme.typography.headlineSmall,
-                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(5.dp),
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        AsyncImage(
+                            model = iconModel,
+                            contentDescription = stringResource(R.string.image),
+                            modifier = Modifier
+                                .padding(5.dp)
+                                .width(30.dp)
+                                .height(30.dp)
+                                .clip(RoundedCornerShape(6.dp))
+                        )
+                        Text(
+                            text = description,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            style = MaterialTheme.typography.headlineMedium,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
                     Spacer(modifier = Modifier.height(5.dp))
                     HeaderFieldsView(
                         headerFields = headerFields,
                         cardColors = CardDefaults.elevatedCardColors()
                     )
                 }
-                Spacer(modifier = Modifier.width(5.dp))
-                AsyncPassImage(
-                    model = iconModel,
-                    modifier = Modifier
-                        .padding(5.dp)
-                        .align(Alignment.CenterVertically)
-                        .weight(2f)
-                        .width(100.dp)
-                        .height(100.dp)
-                )
             }
             Row(
                 horizontalArrangement = Arrangement.spacedBy(10.dp),
