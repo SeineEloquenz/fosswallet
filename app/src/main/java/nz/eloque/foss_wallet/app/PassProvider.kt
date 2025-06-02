88b9efd24719132fad56087c69f6e6ead32566d5
Alexander Linder <seine@eloque.nz>
2025-06-02 17:09:12 +0200

    Remove unused class body

--- app/src/main/java/nz/eloque/foss_wallet/app/PassProvider.kt
+++ app/src/main/java/nz/eloque/foss_wallet/app/PassProvider.kt
@@ -3,5 +3,4 @@ package nz.eloque.foss_wallet.app
 import androidx.core.content.FileProvider
 import nz.eloque.foss_wallet.R
 
-class PassProvider : FileProvider(R.xml.provider_paths) {
-}
+class PassProvider : FileProvider(R.xml.provider_paths)
