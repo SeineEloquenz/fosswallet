0bf8526cded5b60dadb0e866c4ce1440dfe4e284
Alexander Linder <seine@eloque.nz>
2025-06-06 22:42:10 +0200

    Add undo search button to FilterBar

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/FilterBar.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/FilterBar.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Undo
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
@@ -16,8 +18,10 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nz.eloque.foss_wallet.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -44,6 +48,18 @@ fun FilterBar(
                 onExpandedChange = {},
                 expanded = false,
                 modifier = Modifier.fillMaxHeight(),
+                trailingIcon = {
+                    if (query.isNotEmpty()) {
+                        IconButton(
+                            onClick = {
+                                query = ""
+                                onSearch.invoke("")
+                            },
+                        ) {
+                            Icon(imageVector = Icons.Default.Undo, contentDescription = stringResource(R.string.delete))
+                        }
+                    }
+                }
             )
         },
         expanded = false,
