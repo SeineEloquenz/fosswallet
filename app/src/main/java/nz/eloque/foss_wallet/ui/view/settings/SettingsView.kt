fe840d5e152e3adcb980ad2047ad9a23d31658c6
Alexander Linder <seine@eloque.nz>
2025-06-12 17:10:28 +0200

    Improve settings styling

--- app/src/main/java/nz/eloque/foss_wallet/ui/view/settings/SettingsView.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/view/settings/SettingsView.kt
@@ -1,14 +1,23 @@
 package nz.eloque.foss_wallet.ui.view.settings
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Save
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import nz.eloque.foss_wallet.R
@@ -23,24 +32,50 @@ fun SettingsView(
     val coroutineScope = rememberCoroutineScope()
     val settings = settingsViewModel.uiState.collectAsState()
 
-    Column {
-        SettingsSwitch(
-            name = R.string.enable_sync,
-            switchState = settings.value.enableSync,
-            onCheckedChange = { coroutineScope.launch(Dispatchers.IO) { settingsViewModel.enableSync(it) } }
-        )
-        SubmittableTextField(
-            label = { Text(stringResource(R.string.sync_interval)) },
-            initialValue = settings.value.syncInterval.inWholeMinutes.toString(),
-            imageVector = Icons.Default.Save,
-            inputValidator = { isInt(it) },
-            onSubmit = {
-                coroutineScope.launch(Dispatchers.IO) { settingsViewModel.setSyncInterval(Integer.parseInt(it).toDuration(
-                    DurationUnit.MINUTES)) }
-            },
-            enabled = settings.value.enableSync,
-            clearOnSubmit = false,
-        )
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(8.dp)
+    ) {
+        SettingsSection(
+            heading = stringResource(R.string.enable_sync),
+        ) {
+            SettingsSwitch(
+                name = R.string.enable,
+                switchState = settings.value.enableSync,
+                onCheckedChange = { coroutineScope.launch(Dispatchers.IO) { settingsViewModel.enableSync(it) } }
+            )
+            HorizontalDivider()
+            SubmittableTextField(
+                label = { Text(stringResource(R.string.sync_interval)) },
+                initialValue = settings.value.syncInterval.inWholeMinutes.toString(),
+                imageVector = Icons.Default.Save,
+                inputValidator = { isInt(it) },
+                onSubmit = {
+                    coroutineScope.launch(Dispatchers.IO) { settingsViewModel.setSyncInterval(Integer.parseInt(it).toDuration(
+                        DurationUnit.MINUTES)) }
+                },
+                enabled = settings.value.enableSync,
+                clearOnSubmit = false,
+            )
+        }
+    }
+}
+
+@Composable
+fun SettingsSection(
+    heading: String,
+    content: @Composable () -> Unit,
+) {
+    Text(
+        text = heading
+    )
+    Spacer(modifier = Modifier.height(8.dp))
+    ElevatedCard {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            content()
+        }
     }
 }
 
