dd7750e9aaf3cea1919f70d3f129f563bc4ba081
Alexander Linder <34923333+SeineEloquenz@users.noreply.github.com>
2025-09-04 18:00:39 +0200

    Improve selection indication (#260)

--- app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
@@ -1,16 +1,19 @@
 package nz.eloque.foss_wallet.ui.card
 
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.tooling.preview.Preview
 import nz.eloque.foss_wallet.model.Pass
+import nz.eloque.foss_wallet.ui.components.SelectionIndicator
 
 @Composable
 fun ShortPassCard(
@@ -22,24 +25,34 @@ fun ShortPassCard(
 ) {
     val cardColors = pass.colors?.toCardColors() ?: colors
     val scale by animateFloatAsState(if (selected) 0.95f else 1f)
-    if (onClick == null) {
-        ElevatedCard(
-            colors = cardColors,
-            modifier = modifier
-                .fillMaxWidth()
-                .scale(scale)
-        ) {
-            ShortPassContent(pass, cardColors)
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        if (onClick == null) {
+            ElevatedCard(
+                colors = cardColors,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .scale(scale)
+            ) {
+                ShortPassContent(pass, cardColors)
+            }
+        } else {
+            ElevatedCard(
+                onClick = onClick,
+                colors = cardColors,
+                modifier = modifier
+                    .fillMaxWidth()
+                    .scale(scale)
+            ) {
+                ShortPassContent(pass, cardColors)
+            }
         }
-    } else {
-        ElevatedCard(
-            onClick = onClick,
-            colors = cardColors,
-            modifier = modifier
-                .fillMaxWidth()
-                .scale(scale)
-        ) {
-            ShortPassContent(pass, cardColors)
+
+        if (selected) {
+            SelectionIndicator(Modifier.align(Alignment.TopEnd))
         }
     }
 }
