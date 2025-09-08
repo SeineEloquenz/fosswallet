c1c9546ac61fcf1984f5abf6457006dde64a88d4
JSchmerling <mail@schmerl.ing>
2025-09-08 18:45:25 +0200

    Update PassScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/pass/PassScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/pass/PassScreen.kt
@@ -1,284 +1,133 @@
-package nz.eloque.foss_wallet.ui.screens.pass
+package nz.eloque.foss_wallet.ui.screens.wallet
 
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AppShortcut
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.PushPin
-import androidx.compose.material.icons.filled.Square
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Sync
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavHostController
-import kotlinx.coroutines.Dispatchers
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import nz.eloque.foss_wallet.R
-import nz.eloque.foss_wallet.api.FailureReason
-import nz.eloque.foss_wallet.api.UpdateContent
+import nz.eloque.foss_wallet.api.ImportResult
 import nz.eloque.foss_wallet.api.UpdateResult
 import nz.eloque.foss_wallet.model.Pass
-import nz.eloque.foss_wallet.shortcut.Shortcut
-import nz.eloque.foss_wallet.ui.AllowOnLockscreen
-import nz.eloque.foss_wallet.ui.WalletScaffold
-import nz.eloque.foss_wallet.ui.screens.wallet.PassViewModel
-import nz.eloque.foss_wallet.utils.Biometric
-import nz.eloque.foss_wallet.utils.asString
+import nz.eloque.foss_wallet.model.PassWithLocalization
+import nz.eloque.foss_wallet.persistence.BarcodePosition
+import nz.eloque.foss_wallet.persistence.PassStore
+import nz.eloque.foss_wallet.persistence.SettingsStore
+import nz.eloque.foss_wallet.persistence.loader.PassLoadResult
 import java.util.Locale
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun PassScreen(
-    passId: String,
-    navController: NavHostController,
-    passViewModel: PassViewModel
-) {
-    val coroutineScope = rememberCoroutineScope()
-    val pass = remember { mutableStateOf(Pass.placeholder())}
-    LaunchedEffect(coroutineScope) {
-        coroutineScope.launch(Dispatchers.IO) {
-            pass.value = passViewModel.passById(passId).applyLocalization(Locale.getDefault().language)
+data class PassUiState(
+    var isAuthenticated: Boolean = false,
+    var isHidden: Boolean = false,
+    var isPinned: Boolean = false,
+    val query: String = "",
+    val passes: List<Pass> = ArrayList()
+)
+
+@HiltViewModel
+class PassViewModel @Inject constructor(
+    application: Application,
+    private val passStore: PassStore,
+    private val settingsStore: SettingsStore,
+    private val workManager: WorkManager
+) : AndroidViewModel(application) {
+
+    private val _uiState = MutableStateFlow(PassUiState())
+    val uiState: StateFlow<PassUiState> = _uiState.asStateFlow()
+    private val workData: LiveData<List<WorkInfo>>
+    private val observer = { workInfo: List<WorkInfo> -> updatePasses() }
+
+    init {
+        updatePasses()
+        workData = workManager.getWorkInfosByTagLiveData("update")
+        workData.observeForever(observer)
+    }
+
+    override fun onCleared() {
+        workData.removeObserver(observer)
+    }
+
+    private fun updatePasses() {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(passes = passStore.allPasses(true).first().map { it.applyLocalization(
+                Locale.getDefault().language) })
         }
     }
 
-    AllowOnLockscreen {
-        val snackbarHostState = remember { SnackbarHostState() }
-        WalletScaffold(
-            snackbarHostState = snackbarHostState,
-            navController = navController,
-            title = pass.value.description,
-            toolWindow = true,
-            actions = {
-                Actions(pass, navController, snackbarHostState, passViewModel)
-            },
-        ) { scrollBehavior ->
-            PassView(pass.value, passViewModel.barcodePosition(), scrollBehavior = scrollBehavior, increaseBrightness =  passViewModel.increasePassViewBrightness())
+    fun passById(id: String): PassWithLocalization = passStore.passById(id).apply { updatePasses() }
+
+    fun group(passes: Set<Pass>) = passStore.group(passes).apply { updatePasses() }
+
+    fun deleteGroup(groupId: Long) = passStore.deleteGroup(groupId).apply { updatePasses() }
+
+    fun filter(query: String, authStatus: Boolean) {
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(query = query, passes = passStore.filtered(query, authStatus).first().map { it.applyLocalization(Locale.getDefault().language) })
         }
     }
-}
 
-@Composable
-fun Actions(
-    pass: MutableState<Pass>,
-    navController: NavHostController,
-    snackbarHostState: SnackbarHostState,
-    passViewModel: PassViewModel
-) {
-    val context = LocalContext.current
-    val activity = remember(context) { context as FragmentActivity }
-    val coroutineScope = rememberCoroutineScope()
+    fun add(loadResult: PassLoadResult): ImportResult = passStore.add(loadResult).apply { updatePasses() }
 
-    val biometric = remember { Biometric(activity, snackbarHostState, coroutineScope) }
+    suspend fun update(pass: Pass): UpdateResult = passStore.update(pass).apply { updatePasses() }
 
-    val expanded = remember { mutableStateOf(false) }
-    val isLoading = remember { mutableStateOf(false) }
-    val uiState by passViewModel.uiState.collectAsStateWithLifecycle()
+    fun delete(pass: Pass) = passStore.delete(pass).apply { updatePasses() }
 
-    Box(
-        modifier = Modifier
-            .padding(16.dp)
-    ) {
-        IconButton(onClick = { expanded.value = !expanded.value }) {
-            Icon(Icons.Default.MoreVert, contentDescription = stringResource(R.string.more_options))
-        }
-        DropdownMenu(
-            expanded = expanded.value,
-            onDismissRequest = { expanded.value = false }
-        ) {
-            uiState.isPinned = passViewModel.pinned(pass.value)
-            key(uiState.isPinned) {
-                if (passViewModel.pinned(pass.value)) {
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.unpin)) },
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.Square,
-                                contentDescription = stringResource(R.string.unpin)
-                            )
-                        },
-                        onClick = { passViewModel.unpin(pass.value) }
-                    )
-                } else {
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.pin)) },
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.PushPin,
-                                contentDescription = stringResource(R.string.pin)
-                            )
-                        },
-                        onClick = { passViewModel.pin(pass.value) }
-                    )
-                }
-            }
+    fun load(context: Context, bytes: ByteArray): ImportResult = passStore.load(context, bytes).apply { updatePasses() }
+    fun associate(groupId: Long, passes: Set<Pass>) = passStore.associate(groupId, passes).apply { updatePasses() }
+    fun dissociate(pass: Pass, groupId: Long) = passStore.dissociate(pass, groupId).apply { updatePasses() }
 
-            DropdownMenuItem(
-                text = { Text(stringResource(R.string.add_shortcut)) },
-                leadingIcon =  {
-                    Icon(imageVector = Icons.Default.AppShortcut, contentDescription = stringResource(R.string.add_shortcut))
-                },
-                onClick = {
-                    Shortcut.create(context, pass.value, pass.value.description)
-                }
-            )
+    fun archive(pass: Pass) = passStore.archive(pass).apply { updatePasses() }
+    fun unarchive(pass: Pass) = passStore.unarchive(pass).apply { updatePasses() }
 
-            val passFile = pass.value.originalPassFile(context)
-            if (passFile != null) {
-                PassShareButton(passFile)
-            }
-
-            if (pass.value.updatable()) {
-                val uriHandler = LocalUriHandler.current
-                UpdateButton(isLoading = isLoading.value) {
-                    coroutineScope.launch(Dispatchers.IO) {
-                        isLoading.value = true
-                        val result = passViewModel.update(pass.value)
-                        isLoading.value = false
-                        when (result) {
-                            is UpdateResult.Success -> if (result.content is UpdateContent.Pass) {
-                                pass.value = result.content.pass
-                                snackbarHostState.showSnackbar(
-                                    message = context.getString(R.string.update_successful),
-                                    duration = SnackbarDuration.Short
-                                )
-                            }
-                            is UpdateResult.NotUpdated -> snackbarHostState.showSnackbar(message = context.getString(R.string.status_not_updated))
-                            is UpdateResult.Failed -> {
-                                val snackResult = snackbarHostState.showSnackbar(
-                                    message = when (result.reason) {
-                                        is FailureReason.Status -> context.getString(result.reason.messageId, result.reason.status)
-                                        else -> context.getString(result.reason.messageId)
-                                    },
-                                    actionLabel = if (result.reason is FailureReason.Detailed) context.getString(R.string.details) else null,
-                                    duration = SnackbarDuration.Short
-                                )
-                                if (snackResult == SnackbarResult.ActionPerformed && result.reason is FailureReason.Detailed) {
-                                    when (result.reason) {
-                                        is FailureReason.Exception -> coroutineScope.launch(Dispatchers.Main) { navController.navigate("updateFailure/${result.reason.exception.message}/${result.reason.exception.asString()}") }
-                                        is FailureReason.Status -> uriHandler.openUri("https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/${result.reason.status}")
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            uiState.isHidden = passViewModel.hidden(pass.value)
-            key(uiState.isAuthenticated, uiState.isHidden) {
-                if (passViewModel.hidden(pass.value)) {
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.unhide)) },
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.Visibility,
-                                contentDescription = stringResource(R.string.unhide)
-                            )
-                        },
-                        onClick = {
-                            if (uiState.isAuthenticated) {
-                                passViewModel.unhide(pass.value)
-                            } else {
-                                biometric.showBiometricPrompt(
-                                    description = context.getString(R.string.unhide),
-                                    onSuccess = { passViewModel.unhide(pass.value) }
-                                )
-                            }
-                        }
-                    )
-                } else {
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.hide)) },
-                        leadingIcon = {
-                            Icon(
-                                imageVector = Icons.Default.VisibilityOff,
-                                contentDescription = stringResource(R.string.hide)
-                            )
-                        },
-                        onClick = {
-                            if (uiState.isAuthenticated) {
-                                passViewModel.hide(pass.value)
-                            } else {
-                                biometric.showBiometricPrompt(
-                                    description = context.getString(R.string.hide),
-                                    onSuccess = { passViewModel.hide(pass.value) }
-                                )
-                            }
-                        }
-                    )
-                }
-            }
-
-            DropdownMenuItem(
-                text = { Text(stringResource(R.string.delete), color = MaterialTheme.colorScheme.error) },
-                leadingIcon =  {
-                    Icon(imageVector = Icons.Default.Delete, contentDescription = stringResource(R.string.delete), tint = MaterialTheme.colorScheme.error)
-                },
-                onClick = {
-                    coroutineScope.launch(Dispatchers.IO) { passViewModel.delete(pass.value) }
-                    navController.popBackStack()
-                }
-            )
+    fun hide(pass: Pass) {
+        passStore.hide(pass)
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isHidden = true)
+            updatePasses()
         }
     }
-}
+    
+    fun unhide(pass: Pass) {
+        passStore.unhide(pass)
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isHidden = false)
+            updatePasses()
+        }
+    }
 
-@Composable
-fun UpdateButton(
-    isLoading: Boolean,
-    onClick: () -> Unit,
-) {
-    val infiniteTransition = rememberInfiniteTransition()
-    val rotation by infiniteTransition.animateFloat(
-        initialValue = 360f,
-        targetValue = 0f,
-        animationSpec = infiniteRepeatable(
-            animation = tween(1500, easing = FastOutSlowInEasing),
-            repeatMode = RepeatMode.Restart
-        ),
-        label = "spin"
-    )
+    fun pin(pass: Pass) {
+        passStore.pin(pass)
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isPinned = true)
+            updatePasses()
+        }
+    }
+    
+    fun unpin(pass: Pass) {
+        passStore.unpin(pass)
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isPinned = false)
+            updatePasses()
+        }
+    }
 
-    DropdownMenuItem(
-        text = { Text(stringResource(R.string.update)) },
-        leadingIcon = {
-            Icon(imageVector = Icons.Default.Sync, contentDescription = stringResource(R.string.update), modifier = Modifier.graphicsLayer(
-                rotationZ = if (isLoading) rotation else 0f
-            ))
-        },
-        onClick = { if (!isLoading) onClick() },
-    )
+    fun pinned(pass: Pass) = passStore.pinned(pass).apply { updatePasses() }
+
+    fun reveal() { _uiState.value = _uiState.value.copy(isAuthenticated = true) }
+    
+    fun conceal() { _uiState.value = _uiState.value.copy(isAuthenticated = false) }
+
+    fun hidden(pass: Pass) = passStore.hidden(pass).apply { updatePasses() }
+
+    fun barcodePosition(): BarcodePosition = settingsStore.barcodePosition()
+
+    fun increasePassViewBrightness(): Boolean = settingsStore.increasePassViewBrightness()
 }
