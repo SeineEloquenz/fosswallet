4dad171657411f37a8bf276bde4139023063eda4
JSchmerling <mail@schmerl.ing>
2025-09-13 21:58:53 +0200

    Update PassScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/pass/PassScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/pass/PassScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
