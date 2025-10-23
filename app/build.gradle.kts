c21482e705bcce667c1bb2573a91cb5e78eedc98
Alexander Linder <seine@eloque.nz>
2025-10-23 18:24:08 +0200

    Bump version to v0.30.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ android {
         applicationId = "nz.eloque.foss_wallet"
         minSdk = 28
         targetSdk = 36
-        versionCode = 81
-        versionName = "0.29.0"
+        versionCode = 82
+        versionName = "0.30.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
