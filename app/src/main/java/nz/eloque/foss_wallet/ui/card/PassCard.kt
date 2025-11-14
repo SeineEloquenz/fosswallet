a18ac444df7eb3c4337425cdfe3d26165ee62fa1
JSchmerling <mail@schmerl.ing>
2025-11-15 00:01:44 +0100

    Add PassCard composable and preview function

--- app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
@@ -1,3 +1,26 @@
+package nz.eloque.foss_wallet.ui.card
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import nz.eloque.foss_wallet.model.Pass
+import nz.eloque.foss_wallet.ui.components.SelectionIndicator
+
 @Composable
 fun ShortPassCard(
     pass: Pass,
@@ -82,5 +105,15 @@ fun PassCard(
             )
     ) {
         PassContent(pass, cardColors, Modifier, content)
+    }
+}
+
+@Preview
+@Composable
+private fun PasscardPreview() {
+    PassCard(
+        pass = Pass.placeholder(),
+    ) {
+
     }
 }
