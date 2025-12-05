6f354451683821e8f77faf9bfc4644edfe41324a
JSchmerling <mail@schmerl.ing>
2025-12-05 22:50:03 +0100

    Add darken function to Color class

--- app/src/main/java/nz/eloque/foss_wallet/utils/ExtensionFunctions.kt
+++ app/src/main/java/nz/eloque/foss_wallet/utils/ExtensionFunctions.kt
@@ -62,6 +62,15 @@ private fun ZonedDateTime.pretty(dateFormatter: DateTimeFormatter, ignoresTimezo
     }
 }
 
+fun Color.darken(factor: Float = 0.3f): Color {
+    return copy(
+        red = red * factor,
+        green = green * factor,
+        blue = blue * factor,
+        alpha = alpha
+    )
+}
+
 fun Color.tone(degree: Float = 0.3f): Color {
     val factor = if (luminance() < 0.5f) 2 - degree else degree
     return copy(
