f2f6ae3e6edc2e948c3814add69424c8397baf66
JSchmerling <mail@schmerl.ing>
2025-12-18 18:43:20 +0100

    Wrap ShortPassContent in DisableSelection

--- app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/card/PassCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.selection.DisableSelection
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
@@ -58,7 +59,7 @@ fun ShortPassCard(
                     }
                 )
         ) {
-            ShortPassContent(pass, cardColors)
+            DisableSelection(ShortPassContent(pass, cardColors))
         }
         if (selected) {
             SelectionIndicator(Modifier.align(Alignment.TopEnd))
