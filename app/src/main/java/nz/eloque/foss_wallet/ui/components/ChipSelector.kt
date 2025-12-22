ee46996af11b049bf167a9e6fc130a2e11068462
Alexander Linder <seine@eloque.nz>
2025-12-22 23:19:38 +0100

    Add chips to filter wallet by pass type

---
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/ChipSelector.kt
@@ -0,0 +1,43 @@
+package nz.eloque.foss_wallet.ui.components
+
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun <T> ChipSelector(
+    options: List<T>,
+    selectedOptions: List<T>,
+    onOptionSelected: (T) -> Unit,
+    onOptionDeselected: (T) -> Unit,
+    optionLabel: (T) -> String
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier
+            .horizontalScroll(rememberScrollState())
+            .fillMaxWidth()
+    ) {
+        options.forEach { option ->
+            val selected = selectedOptions.contains(option)
+            FilterChip(
+                selected = selected,
+                onClick = {
+                    if (selected) {
+                        onOptionDeselected(option)
+                    } else {
+                        onOptionSelected(option)
+                    }
+                },
+                label = { Text(optionLabel(option)) },
+            )
+        }
+    }
+}
