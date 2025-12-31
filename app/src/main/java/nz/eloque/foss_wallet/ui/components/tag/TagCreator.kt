870832ba95537a0f35c5dea4cbdcb399897ff904
Alexander Linder <seine@eloque.nz>
2025-12-31 15:11:50 +0100

    Add color picker to tag creator

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
@@ -2,8 +2,9 @@ package nz.eloque.foss_wallet.ui.components.tag
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -13,8 +14,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.github.skydoves.colorpicker.compose.HsvColorPicker
+import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.Tag
 
@@ -23,21 +27,28 @@ fun TagCreator(
     onCreate: (Tag) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val controller = rememberColorPickerController()
 
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier,
     ) {
-        val background = MaterialTheme.colorScheme.onSurface
-
         var label by remember { mutableStateOf("") }
-        var color by remember { mutableStateOf(background) }
+        var color by remember { mutableStateOf(Color.White) }
 
         OutlinedTextField(
             label = { Text(stringResource(R.string.tag_label)) },
             value = label,
             onValueChange = { label = it }
+        )
+
+        HsvColorPicker(
+            controller = controller,
+            initialColor = color,
+            onColorChanged = { color = it.color },
+            modifier = Modifier.fillMaxWidth()
+                .height(150.dp)
         )
 
         Button(onClick = {
