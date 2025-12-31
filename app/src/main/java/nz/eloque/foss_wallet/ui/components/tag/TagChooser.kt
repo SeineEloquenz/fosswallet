a0020ffc081c83d6c248f24e3df8b80c15f7f06f
Alexander Linder <seine@eloque.nz>
2025-12-31 15:30:14 +0100

    Use dialog for embedded tag creator

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagChooser.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagChooser.kt
@@ -1,14 +1,27 @@
 package nz.eloque.foss_wallet.ui.components.tag
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import nz.eloque.foss_wallet.R
 import nz.eloque.foss_wallet.model.Tag
 
 @Composable
@@ -18,6 +31,8 @@ fun TagChooser(
     onTagCreate: (Tag) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var creatorShown by remember { mutableStateOf(false) }
+
     LazyColumn(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(4.dp),
@@ -38,9 +53,36 @@ fun TagChooser(
         item { HorizontalDivider() }
 
         item {
-            TagCreator(
-                onCreate = onTagCreate,
-            )
+            Button(onClick = {
+                creatorShown = true
+            }) {
+                Text(stringResource(R.string.add_tag))
+            }
+        }
+    }
+
+    if (creatorShown) {
+        Dialog(
+            onDismissRequest = { creatorShown = false }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(20.dp)
+                    )
+            ) {
+                TagCreator(
+                    onCreate = {
+                        onTagCreate(it)
+                        creatorShown = false
+                    },
+                    modifier = Modifier.padding(20.dp)
+                )
+            }
+
         }
     }
 }
