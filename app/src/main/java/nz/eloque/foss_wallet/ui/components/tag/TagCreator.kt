d90bd737402f0b4d9b3d3adc4ad307bb72dfacfa
Alexander Linder <seine@eloque.nz>
2025-12-31 15:19:57 +0100

    Add color preview to color picker

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
@@ -2,8 +2,12 @@ package nz.eloque.foss_wallet.ui.components.tag
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,9 +18,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.github.skydoves.colorpicker.compose.AlphaTile
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
 import nz.eloque.foss_wallet.R
@@ -37,24 +43,44 @@ fun TagCreator(
         var label by remember { mutableStateOf("") }
         var color by remember { mutableStateOf(Color.White) }
 
+        val valid = !label.isEmpty() && label.length < 30
+
         OutlinedTextField(
             label = { Text(stringResource(R.string.tag_label)) },
+            isError = !valid,
             value = label,
             onValueChange = { label = it }
         )
 
-        HsvColorPicker(
-            controller = controller,
-            initialColor = color,
-            onColorChanged = { color = it.color },
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
             modifier = Modifier.fillMaxWidth()
-                .height(150.dp)
-        )
+        ) {
+            HsvColorPicker(
+                controller = controller,
+                initialColor = color,
+                onColorChanged = { color = it.color },
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+            )
+            AlphaTile(
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(6.dp)),
+                controller = controller,
+            )
+        }
 
-        Button(onClick = {
-            val tag = Tag(label, color)
-            onCreate(tag)
-        }) {
+
+        Button(
+            onClick = {
+                val tag = Tag(label, color)
+                onCreate(tag)
+            },
+            enabled = valid
+        ) {
             Text(text = stringResource(R.string.add_tag))
         }
     }
