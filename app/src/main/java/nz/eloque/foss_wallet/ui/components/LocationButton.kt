49e4bcede994726df68a9896e7a623fe2451f256
JSchmerling <mail@schmerl.ing>
2026-01-21 22:01:25 +0100

    Use Snackbar instead of Log for error handling
    Replace logging with Snackbar for error handling when no map app is found.

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/LocationButton.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/LocationButton.kt
@@ -3,15 +3,19 @@ package nz.eloque.foss_wallet.ui.components
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.location.Location
-import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.net.toUri
+import kotlinx.coroutines.launch
+import nz.eloque.foss_wallet.R
 
 
 @Composable
@@ -20,6 +24,8 @@ fun LocationButton(
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
+    val resources = LocalResources.current
+    
     IconButton(
         onClick = {
             try {
@@ -27,7 +33,9 @@ fun LocationButton(
                     it.data = "geo:${location.latitude},${location.longitude}?q=${location.latitude},${location.longitude}".toUri()
                 })
             } catch (e: ActivityNotFoundException) {
-                Log.e("LocationButton", "No map app found!", e)
+                scope.launch {
+                    snackbarHostState.showSnackbar(message = resources.getString(R.string.no_calendar_app_found))
+                }
             }
         },
         modifier = modifier,
