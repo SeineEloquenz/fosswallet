887758c26671f48b2d5df961cd6e66ecaa70fe2f
JSchmerling <mail@schmerl.ing>
2026-01-24 21:48:08 +0100

    Fix URL encoding in navigation

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
@@ -134,7 +134,7 @@ fun WalletScreen(
                                         val string = item?.text.toString()
                                         if (string.startsWith("https://") || string.startsWith("http://")) {
                                             withContext(Dispatchers.Main) {
-                                                navController.navigate("${Screen.Web.route}/${URLEncoder.encode(string, Charsets.UTF_8.name())}")
+                                                navController.navigate("${Screen.Web.route}/${URLEncoder.encode(string, Charsets.UTF_8)}")
                                             }
                                             return@launch
                                         }
