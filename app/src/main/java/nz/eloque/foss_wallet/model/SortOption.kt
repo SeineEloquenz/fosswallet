6a4f5478395c58a921c7de6f41081aa939170189
JSchmerling <mail@schmerl.ing>
2026-01-24 17:17:01 +0100

    Spruce up SortOptions (#473)
    * Refactor TimeAdded comparator in SortOption
    
    * Update timeAdded comparator to use non-null ZonedDateTime
    
    * Update timeAdded comparator to handle nullable ZonedDateTime
    
    * Refactor timeAdded comparator for sorting
    
    Updated timeAdded comparator to use Instant instead of ZonedDateTime.
    
    * Import Instant in SortOption.kt
    
    Added import for Instant to SortOption.kt

--- app/src/main/java/nz/eloque/foss_wallet/model/SortOption.kt
+++ app/src/main/java/nz/eloque/foss_wallet/model/SortOption.kt
@@ -3,15 +3,16 @@ package nz.eloque.foss_wallet.model
 import androidx.annotation.StringRes
 import androidx.compose.runtime.saveable.Saver
 import nz.eloque.foss_wallet.R
+import java.time.Instant
 import java.time.ZonedDateTime
 
 const val TIME_ADDED = "TimeAdded"
 const val RELEVANT_DATE_NEWEST = "RelevantDateNewest"
 const val RELEVANT_DATE_OLDEST = "RelevantDateOldest"
 
-private val oldestFirst = Comparator.comparing<LocalizedPassWithTags, ZonedDateTime?>(
-    { it.pass.relevantDates.firstOrNull()?.startDate() },
-    Comparator.nullsLast(Comparator.naturalOrder())
+private val timeAdded = Comparator.comparing<LocalizedPassWithTags, Instant?>(
+    { it.pass.addedAt },
+    Comparator.reverseOrder()
 )
 
 private val newestFirst = Comparator.comparing<LocalizedPassWithTags, ZonedDateTime?>(
@@ -19,11 +20,13 @@ private val newestFirst = Comparator.comparing<LocalizedPassWithTags, ZonedDateT
     Comparator.nullsLast(Comparator.reverseOrder())
 )
 
+private val oldestFirst = Comparator.comparing<LocalizedPassWithTags, ZonedDateTime?>(
+    { it.pass.relevantDates.firstOrNull()?.startDate() },
+    Comparator.nullsLast(Comparator.naturalOrder())
+)
 
 sealed class SortOption(val name: String, @param:StringRes val l18n: Int, val comparator: Comparator<LocalizedPassWithTags>) {
-    object TimeAdded : SortOption(TIME_ADDED, R.string.date_added, Comparator { left, right ->
-        -left.pass.addedAt.compareTo(right.pass.addedAt)
-    })
+    object TimeAdded : SortOption(TIME_ADDED, R.string.date_added, timeAdded)
     object RelevantDateNewest : SortOption(RELEVANT_DATE_NEWEST, R.string.relevant_date_newest, newestFirst)
     object RelevantDateOldest : SortOption(RELEVANT_DATE_OLDEST, R.string.relevant_date_oldest, oldestFirst)
 
