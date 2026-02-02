fdf214d343bd4891d239787882e54b785193363e
JSchmerling <mail@schmerl.ing>
2026-02-02 19:58:31 +0100

    Update PassParser.kt

--- app/src/main/java/nz/eloque/foss_wallet/parsing/PassParser.kt
+++ app/src/main/java/nz/eloque/foss_wallet/parsing/PassParser.kt
@@ -17,7 +17,7 @@ import nz.eloque.foss_wallet.model.field.PassField
 import nz.eloque.foss_wallet.persistence.loader.InvalidPassException
 import nz.eloque.foss_wallet.persistence.loader.PassBitmaps
 import nz.eloque.foss_wallet.utils.Hash
-import nz.eloque.foss_wallet.utils.LinkifyUrls
+import nz.eloque.foss_wallet.utils.linkifyUrls
 import nz.eloque.foss_wallet.utils.forEach
 import nz.eloque.foss_wallet.utils.map
 import org.json.JSONException
