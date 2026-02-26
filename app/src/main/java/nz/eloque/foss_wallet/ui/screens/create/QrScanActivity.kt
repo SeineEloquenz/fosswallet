24a277ae2710ca3177c326230cd732299a1471dd
Niels <niels.muendler@inf.ethz.ch>
2026-02-26 09:12:55 +0100

    Fix missing super call

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/create/QrScanActivity.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/create/QrScanActivity.kt
@@ -107,6 +107,7 @@ class QrScanActivity : AppCompatActivity() {
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    	super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         captureManager?.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
