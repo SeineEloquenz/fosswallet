41e7ebab855f4876db19a787e91accc9b2ce3bb5
JSchmerling <mail@schmerl.ing>
2026-03-28 11:24:06 +0100

    Update and rename linkifyUrls.kt to linkify.kt

--- app/src/main/java/nz/eloque/foss_wallet/utils/linkify.kt
+++ app/src/main/java/nz/eloque/foss_wallet/utils/linkify.kt
@@ -1,5 +1,7 @@
 package nz.eloque.foss_wallet.utils
 
+fun linkify(text: String): String = linkifyUrls(text)
+
 fun linkifyUrls(text: String): String {
     val urlPattern = Regex(
         """(?<!["'>]|href=")https?://[^\s<>"']+(?<![.,;:!?)])(?![^<]*</a>)""",
