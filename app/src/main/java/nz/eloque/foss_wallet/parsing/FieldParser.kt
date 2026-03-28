f34f61988552df10ee120fb1b91c35f518bc42b1
JSchmerling <mail@schmerl.ing>
2026-03-28 11:27:47 +0100

    Update FieldParser.kt

--- app/src/main/java/nz/eloque/foss_wallet/parsing/FieldParser.kt
+++ app/src/main/java/nz/eloque/foss_wallet/parsing/FieldParser.kt
@@ -1,11 +1,11 @@
 package nz.eloque.foss_wallet.parsing
 
+import java.time.format.FormatStyle
 import nz.eloque.foss_wallet.model.field.PassContent
 import nz.eloque.foss_wallet.model.field.PassField
-import nz.eloque.foss_wallet.utils.linkifyUrls
+import nz.eloque.foss_wallet.utils.linkify
 import nz.eloque.foss_wallet.utils.stringOrNull
 import org.json.JSONObject
-import java.time.format.FormatStyle
 
 object FieldParser {
 
@@ -26,7 +26,7 @@ object FieldParser {
             field.hasDateStyle() && field.hasTimeStyle() -> PassContent.DateTime(TimeParser.parse(value), chooseBetter(field.getDateStyle(), field.getTimeStyle()), field.ignoresTimezone(), field.isRelative())
             field.hasDateStyle() -> PassContent.Date(TimeParser.parse(value), field.getDateStyle(), field.ignoresTimezone(), field.isRelative())
             field.hasTimeStyle() -> PassContent.Time(TimeParser.parse(value), field.getTimeStyle(), field.ignoresTimezone(), field.isRelative())
-            else -> PassContent.Plain(linkifyUrls(value))
+            else -> PassContent.Plain(linkify(value))
         }
 
         return PassField(key, label, content, changeMessage)
