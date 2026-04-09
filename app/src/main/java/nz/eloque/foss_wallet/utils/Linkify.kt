c540d68ca4252ade1cce11286169b4a2c483a1e2
JSchmerling <mail@schmerl.ing>
2026-04-09 23:25:42 +0200

    Update linkify function to use PatternsCompat
    Refactor linkify function to use PatternsCompat for URL and email linkification.

--- app/src/main/java/nz/eloque/foss_wallet/utils/Linkify.kt
+++ app/src/main/java/nz/eloque/foss_wallet/utils/Linkify.kt
@@ -1,14 +1,21 @@
 package nz.eloque.foss_wallet.utils
 
-fun linkifyUrls(text: String): String {
-    val urlPattern =
-        Regex(
-            """(?<!["'>]|href=")https?://[^\s<>"']+(?<![.,;:!?)])(?![^<]*</a>)""",
-            RegexOption.IGNORE_CASE,
-        )
+import androidx.core.util.PatternsCompat
 
-    return urlPattern.replace(text) { matchResult ->
-        val url = matchResult.value
-        """<a href="$url" data-linkified="true">$url</a>"""
+fun linkify(text: String): String = linkifyUrls(linkifyMails(text))
+
+fun linkifyUrls(text: String): String {
+    return PatternsCompat.WEB_URL.toRegex(RegexOption.IGNORE_CASE).replace(text) {
+        if (it.value.contains("data-linkified")) return@replace it.value
+        val href = if (!it.value.startsWith("http", ignoreCase = true)) "https://${it.value}" else it.value
+        """<a href="$href" data-linkified="true">${it.value}</a>"""
+    }
+}
+
+fun linkifyMails(text: String): String {
+    return PatternsCompat.EMAIL_ADDRESS.toRegex(RegexOption.IGNORE_CASE).replace(text) {
+        if (it.value.contains("data-linkified")) return@replace it.value
+        val mail = it.value.removePrefix("mailto:")
+        """<a href="mailto:$mail" data-linkified="true">$mail</a>"""
     }
 }
