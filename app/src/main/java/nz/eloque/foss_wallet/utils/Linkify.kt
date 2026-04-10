e23420d1b57af904384d1e9490eb0ed2afb413cc
JSchmerling <mail@schmerl.ing>
2026-04-10 09:10:02 +0200

    Refactor linkify functions to use Regex constructor

--- app/src/main/java/nz/eloque/foss_wallet/utils/Linkify.kt
+++ app/src/main/java/nz/eloque/foss_wallet/utils/Linkify.kt
@@ -5,7 +5,7 @@ import androidx.core.util.PatternsCompat
 fun linkify(text: String): String = linkifyUrls(linkifyMails(text))
 
 fun linkifyUrls(text: String): String {
-    return PatternsCompat.WEB_URL.toRegex(RegexOption.IGNORE_CASE).replace(text) {
+    return Regex(PatternsCompat.WEB_URL.pattern(), RegexOption.IGNORE_CASE).replace(text) {
         if (it.value.contains("data-linkified")) return@replace it.value
         val href = if (!it.value.startsWith("http", ignoreCase = true)) "https://${it.value}" else it.value
         """<a href="$href" data-linkified="true">${it.value}</a>"""
@@ -13,7 +13,7 @@ fun linkifyUrls(text: String): String {
 }
 
 fun linkifyMails(text: String): String {
-    return PatternsCompat.EMAIL_ADDRESS.toRegex(RegexOption.IGNORE_CASE).replace(text) {
+    return Regex(PatternsCompat.EMAIL_ADDRESS.pattern(), RegexOption.IGNORE_CASE).replace(text) {
         if (it.value.contains("data-linkified")) return@replace it.value
         val mail = it.value.removePrefix("mailto:")
         """<a href="mailto:$mail" data-linkified="true">$mail</a>"""
