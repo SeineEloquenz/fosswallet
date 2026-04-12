44de9cd0ba07492c24396049397c8ed6e1b5d6b5
JSchmerling <mail@schmerl.ing>
2026-04-12 21:15:45 +0200

    Refactor SelectionMenu for multi and single options

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/SelectionMenu.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/SelectionMenu.kt
@@ -4,52 +4,74 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.FilterList
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.RadioButtonChecked
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import nz.eloque.foss_wallet.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun <T> SelectionMenu(
-    options: List<T>,
-    selectedOption: T,
+fun <T, F> SelectionMenu(
+    multiOptions: Collection<F>,
+    singleOptions: List<T>,
+    multiOptionLabel: (T) -> String,
+    singleOptionLabel: (F) -> String,
+    selectedMultiOptions: Collection<F>,
+    selectedSingleOption: T,
+    onMultiOptionSelected: (F) -> Unit,
+    onSingleOptionSelected: (T) -> Unit,
+    onSingleOptionDeselected: (F) -> Unit,
     modifier: Modifier = Modifier,
-    icon: ImageVector = Icons.Default.Menu,
     @StringRes contentDescription: Int = R.string.more_options,
-    onOptionSelected: (T) -> Unit,
-    optionLabel: (T) -> String,
 ) {
-    val expanded = remember { mutableStateOf(false) }
-    Box(
-        modifier = modifier,
-    ) {
-        IconButton(onClick = { expanded.value = !expanded.value }) {
-            Icon(icon, contentDescription = stringResource(contentDescription))
+    var expanded by remember { mutableStateOf(false) }
+
+    Box(modifier = modifier) {
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(Icons.Default.Menu, contentDescription = stringResource(contentDescription))
         }
         DropdownMenu(
-            expanded = expanded.value,
-            onDismissRequest = { expanded.value = false },
+            expanded = expanded,
+            onDismissRequest = { expanded = false }
         ) {
-            options.forEach { option ->
+            multiOptions.forEach { option -> // multi select
+                val selected = selectedMultiOptions.contains(option)
                 DropdownMenuItem(
-                    text = { Text(optionLabel(option)) },
-                    trailingIcon = {
-                        if (option == selectedOption) {
-                            Icon(Icons.Default.Check, stringResource(R.string.selected))
+                    text = { Text(multiOptionLabel(option)) },
+                    leadingIcon = {
+                        if (selected) {
+                            Icon(Icons.Default.Check, contentDescription = stringResource(R.string.selected))
                         }
                     },
-                    onClick = { onOptionSelected(option) },
+                    onClick = { if (selected) onMultiOptionDeselected(option) else onMultiOptionSelected(option) }
+                )
+            }
+
+            HorizontalDivider()
+
+            singleOptions.forEach { option -> // single select
+                DropdownMenuItem(
+                    text = { Text(singleOptionLabel(option)) },
+                    leadingIcon = {
+                        if (option == selectedSingleOption) {
+                            Icon(Icons.Default.RadioButtonChecked, stringResource(R.string.selected))
+                        }
+                    },
+                    onClick = { onSingleOptionSelected(option) }
                 )
             }
         }
