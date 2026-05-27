010c294194a5e442507af7d06dbc5b22dd440168
JSchmerling <mail@schmerl.ing>
2026-05-27 22:25:38 +0200

    Update TagCreator.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/tag/TagCreator.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Tag
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
