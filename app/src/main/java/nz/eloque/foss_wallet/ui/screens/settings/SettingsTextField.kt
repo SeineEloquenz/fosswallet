ff1583728fe5978954dd7ad550947b0dde78cbe3
JSchmerling <mail@schmerl.ing>
2026-06-14 16:29:54 +0200

    Remove unused import for KeyboardActions

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/settings/SettingsTextField.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/settings/SettingsTextField.kt
@@ -3,7 +3,6 @@ package nz.eloque.foss_wallet.ui.screens.settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
