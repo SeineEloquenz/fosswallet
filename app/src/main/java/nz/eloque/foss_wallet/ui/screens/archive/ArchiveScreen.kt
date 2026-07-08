ad9f1a9f3d6ac2d9bec49c2eeea45fd738f0e816
JSchmerling <mail@schmerl.ing>
2026-07-08 23:36:59 +0200

    Add collectAsState import to ArchiveScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/archive/ArchiveScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/archive/ArchiveScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
