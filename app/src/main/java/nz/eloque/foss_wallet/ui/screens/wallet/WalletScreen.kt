57cc548205cc85743aaa351b4a69b026bc4d962f
JSchmerling <mail@schmerl.ing>
2026-07-09 09:48:23 +0200

    Add missing import for getValue in WalletScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/WalletScreen.kt
@@ -20,6 +20,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
