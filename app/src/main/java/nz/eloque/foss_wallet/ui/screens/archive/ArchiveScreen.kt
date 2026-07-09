4b3e6c198f149703016868ba992f074d20e57741
JSchmerling <mail@schmerl.ing>
2026-07-09 09:47:57 +0200

    Add import for getValue in ArchiveScreen.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/archive/ArchiveScreen.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/archive/ArchiveScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.mutableStateSetOf
 import androidx.compose.runtime.remember
