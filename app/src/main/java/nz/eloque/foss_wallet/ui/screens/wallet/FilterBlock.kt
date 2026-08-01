0712dfb8fcc4ded593a042324ab2463226d017cf
JSchmerling <mail@schmerl.ing>
2026-08-01 15:35:09 +0200

    Refactor imports in FilterBlock.kt

--- app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/screens/wallet/FilterBlock.kt
@@ -2,15 +2,11 @@ package nz.eloque.foss_wallet.ui.screens.wallet
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.platform.LocalResources
 import androidx.compose.ui.res.stringResource
 import nz.eloque.compose_kit.components.ExtendedSelectionMenu
