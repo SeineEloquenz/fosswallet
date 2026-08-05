faf21d2888235b549ff80fb4346162cc11b89735
JSchmerling <mail@schmerl.ing>
2026-08-05 14:59:19 +0200

    Delete app/src/main/java/nz/eloque/foss_wallet/widget/PassCardGlanceWidgetReceiver.kt

--- app/src/main/java/nz/eloque/foss_wallet/widget/PassCardGlanceWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package nz.eloque.foss_wallet.widget
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class PassCardGlanceWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = PassCardGlanceWidget()
-}
