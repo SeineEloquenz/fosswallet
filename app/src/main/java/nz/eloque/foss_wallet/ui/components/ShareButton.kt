f4364043b7037c1368618c6a85e2bf6d69459ef8
JSchmerling <mail@schmerl.ing>
2026-08-14 20:45:00 +0200

    Add ShareButton composable for file sharing

---
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/ShareButton.kt
@@ -0,0 +1,34 @@
+package nz.eloque.foss_wallet.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import nz.eloque.foss_wallet.R
+import nz.eloque.foss_wallet.share.share
+import java.io.File
+
+@Composable
+fun ShareButton(file: File) {
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    IconButton(
+        onClick = {
+            coroutineScope.launch(Dispatchers.IO) {
+                share(file, context)
+            }
+        }
+    ) {
+        Icon(
+          imageVector = Icons.Default.Share,
+          contentDescription = stringResource(R.string.share),
+        )
+    }
+}
