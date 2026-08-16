c9e9ca3e400c9c77db6c0270823a766e036dab38
JSchmerling <mail@schmerl.ing>
2026-08-16 13:50:43 +0200

    Use LauncherService in LauncherButton
    Refactor LauncherButton to use LauncherService for adding pass.

--- app/src/main/java/nz/eloque/foss_wallet/ui/components/LauncherButton.kt
+++ app/src/main/java/nz/eloque/foss_wallet/ui/components/LauncherButton.kt
@@ -7,13 +7,17 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import nz.eloque.foss_wallet.R
+import nz.eloque.foss_wallet.launcher.LauncherService
+import nz.eloque.foss_wallet.model.Pass
 import java.io.File
 
 @Composable
 fun LauncherButton(pass: Pass) {
+    private val launcherService: LauncherService
+
     IconButton(
         onClick = {
-                passViewModel.addToLauncher(pass)
+                launcherService.create(pass)
             }
         }
     ) {
